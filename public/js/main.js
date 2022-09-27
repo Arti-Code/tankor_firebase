e88db2d9fde793a798d2092b21810651aeced7fe
Arturr0 <gwozdyk@gmail.com.com>
2022-09-28 00:48:51 +0200

    js

--- public/js/main.js
+++ public/js/main.js
@@ -4,47 +4,67 @@ const app = firebase.initializeApp(auth);
 const db = firebase.database();
 
 const messages = document.getElementById('messages');
-let localDescription = document.getElementById('localSessionDescription');
+//let localDescription = document.getElementById('localSessionDescription');
 let btnStart = document.getElementById('btn_start');
 let btnStop = document.getElementById('btn_stop');
+let stat_disconnected = document.getElementById('status_disconnected');
+let stat_connecting = document.getElementById('status_connecting');
+let stat_connected = document.getElementById('status_connected');
 let device_id = "kamera";
+let pc;
 
-btnStart.addEventListener('click', wait_answer)
+btnStart.addEventListener('click', init)
 btnStop.addEventListener('click', stopConnection);
 btnStart.addEventListener('touchend', wait_answer)
 btnStop.addEventListener('touchend', stopConnection);
 
-let pc = new RTCPeerConnection({iceServers: [{urls: 'stun:stun.l.google.com:19302'}]});
+clearNegotiation()
+status_disconnected();
 
-pc.ontrack = function (event) {
-  var el = document.createElement(event.track.kind)
-  el.srcObject = event.streams[0]
-  el.autoplay = true
-  el.controls = true
-  document.getElementById('remoteVideos').appendChild(el)
-}
 
-pc.oniceconnectionstatechange = e => info(pc.iceConnectionState)
+function init() {
+  pc = new RTCPeerConnection({iceServers: [{urls: 'stun:stun.l.google.com:19302'}]});
+  pc.addTransceiver('video', {'direction': 'recvonly'});
+  
+  pc.ontrack = function (event) {
+    var el = document.createElement(event.track.kind)
+    el.srcObject = event.streams[0]
+    el.autoplay = true
+    el.controls = true
+    document.getElementById('remoteVideos').appendChild(el)
+  };
+  
+  pc.oniceconnectionstatechange = function (event) {
+    info(pc.iceConnectionState)
+    if (pc.iceConnectionState == 'checking') {
+      status_connecting();
+    } else if (pc.iceConnectionState == 'connected') {
+      status_connected();
+    } else if (pc.iceConnectionState == 'completed') {
+      status_connected();
+    } else {
+      status_disconnected();
+    }
+  };
 
-pc.onicecandidate = event => {
-  if (event.candidate === null) {
-    let lsd = btoa(JSON.stringify(pc.localDescription));
-    console.log("[OFFER]: " + lsd);
-    info("[OFFER] OK");
-    db_write(device_id, "answer", "");
-    db_write(device_id, "offer", lsd);
-    localDescription.value = lsd;
-  }
-}
-
-pc.onnegotiationneeded = event => {
-  pc.createOffer().then(d => pc.setLocalDescription(d)).catch(info);
+  pc.onicecandidate = function (event) {
+    if (event.candidate === null) {
+      let lsd = btoa(JSON.stringify(pc.localDescription));
+      console.log("[OFFER]: " + "ok");
+      info("[OFFER] OK");
+      //db_write(device_id, "answer", "");
+      db_write(device_id, "offer", lsd);
+      console.log("[ANSWER]: " + "waiting");
+      info("[ANSWER] waiting");
+      wait_answer();
+    }
+  };
+  
+  pc.onnegotiationneeded = function (event) {
+    pc.createOffer().then(d => pc.setLocalDescription(d)).catch(info);
+  };
 };
-
-//pc.addTransceiver('audio', {'direction': 'recvonly'})
-pc.addTransceiver('video', {'direction': 'recvonly'})
-//pc.addTransceiver('video', {'direction': 'recvonly'})
-//pc.createOffer().then(d => pc.setLocalDescription(d)).catch(info)
+  
 
 function db_read() {
   let dbRef = db.ref().child("signaling").child("welcome")
@@ -68,6 +88,11 @@ function info(text) {
   messages.innerHTML = messages.innerHTML + text + '<br />';
 };
 
+function clearNegotiation() {
+  db_write(device_id, "answer", "");
+  db_write(device_id, "offer", "");
+}
+
 function wait_answer() {
   let on_answer = db.ref().child("signaling").child(device_id).child("answer");
   on_answer.on('value', (snapshot) => {
@@ -77,6 +102,7 @@ function wait_answer() {
 
 function set_remote_sdp(data) {
   if (data == "") {
+    console.log('empty answer')
     return;
   } else {
     try {
@@ -97,22 +123,26 @@ function stopConnection() {
   pc.close();
   btnStop.setAttribute('hidden', 'true');
   btnStart.removeAttribute('hidden');
-}
-
-//window.startSession = () => {
-//  wait_answer();
-//};
-
-//window.startSession = () => {
-//  let sd = document.getElementById('remoteSessionDescription').value
-//  if (sd === '') {
-//    return alert('Session Description must not be empty')
-//  }
-//
-//  try {
-//    pc.setRemoteDescription(new RTCSessionDescription(JSON.parse(atob(sd))))
-//  } catch (e) {
-//    alert(e)
-//  }
-//}
+  status_disconnected();
+};
   
+function hide_status_flags() {
+  stat_disconnected.setAttribute('hidden', 'true');
+  stat_connecting.setAttribute('hidden', 'true');
+  stat_connected.setAttribute('hidden', 'true');
+};
+
+function status_disconnected() {
+  hide_status_flags();
+  stat_disconnected.removeAttribute('hidden');
+};
+
+function status_connecting() {
+  hide_status_flags();
+  stat_connecting.removeAttribute('hidden');
+};
+
+function status_connected() {
+  hide_status_flags();
+  stat_connected.removeAttribute('hidden');
+};
